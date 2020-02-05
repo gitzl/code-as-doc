cb08d9391d87b2ab282e9fcc5cff6be7c745a2b6
king <1162393020@qq.com>
2020-02-05 15:18:50 +0800

    add springboot project code-as-doc

---
+++ src/main/java/com/example/codes/CodesApplication.java
@@ -0,0 +1,13 @@
+package com.example.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codes/CodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
