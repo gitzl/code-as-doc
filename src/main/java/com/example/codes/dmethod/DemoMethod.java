6856a5a6d640789e646df68ec52c52596882d001
king <1162393020@qq.com>
2020-02-14 11:24:07 +0800

    feat: about method

--- src/main/java/com/example/codes/dmethod/DemoMethod.java
+++ src/main/java/com/example/codes/dmethod/DemoMethod.java
@@ -1,6 +1,9 @@
 package com.example.codes.dmethod;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @Author: LZ
@@ -16,15 +19,29 @@ public class DemoMethod {
      * 3： 不要过于追求提供便利点方法，只有当一项操作经常被重复使用，考虑提供便捷方式
      * 4： 参数尽量少于4个 甚至更少
      * 5： 参考优先考虑接口 再是 类
+     * 6:  使用可变参数  参数不确定非常实用
      *
      *
      *
      */
-    public BigInteger doSomething(BigInteger m) {
+    public BigInteger doSomething(BigInteger m , BigInteger... bigIntegers) {
         if (m.signum() <= 0) {
             throw new ArithmeticException("the sum <=0" + m);
         }
         return m;
+    }
+
+
+    private List<Object> list = new ArrayList<>();
+    public List<Object>  execute () {
+        if (list.size() == 0) {
+            //返回空集合
+            return Collections.EMPTY_LIST;
+
+            //对象为空都集合 建议返回空集合而不是null，返回null 会增加调用者的使用难度
+//            return null;
+        }
+        return list;
     }
 
 }
