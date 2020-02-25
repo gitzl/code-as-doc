a6b1773c9ef52dc69a316be82272ab2f4c896e76
king <1162393020@qq.com>
2020-02-25 15:12:59 +0800

    add volatile

--- src/main/java/com/example/codes/dthread/DemoAtomicInteger.java
+++ src/main/java/com/example/codes/dthread/DemoAtomicInteger.java
@@ -8,8 +8,18 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class DemoAtomicInteger implements  Runnable {
 
+    /*****
+     * 锁：独占锁和共享锁  公平锁（FIFO 先来先得）和非公平
+     * 锁：主要提供2个功能：互斥  可见
+     *
+     *
+     *
+     *
+     *
+     */
 
-     private static int count = 0;
+    private static int count = 0;
+     //synchronized 独占锁
      protected  synchronized void increment() {
          for (int i = 0; i <10000 ; i++) {
              count ++;
---
+++ src/main/java/com/example/codes/dthread/DemoVolatile.java
@@ -0,0 +1,33 @@
+package com.example.codes.dthread;
+
+/**
+ * @Author: LZ
+ * @Date: 2020/2/25 3:00 下午
+ */
+public class DemoVolatile {
+
+//     volatile boolean flag = false;
+    boolean flag = false;
+    protected void doSomething() {
+        while (!flag) {
+            System.out.println("do something ing.......");
+        }
+    }
+
+    protected void shutDown() {
+        flag = true;
+        System.out.println("shut down, stop do something");
+    }
+
+
+    // 不加volatile 出现死循环， thread1改变了变量，但是thread不可见
+    // 可使用 volatile 关键字实现多线程直接 变量可见等
+    public static void main(String[] args) throws InterruptedException {
+        DemoVolatile demoVolatile = new DemoVolatile();
+        Thread thread = new Thread(()->demoVolatile.doSomething());
+        thread.start();
+        Thread.sleep(5000);
+        Thread thread1 = new Thread(() -> demoVolatile.shutDown());
+        thread1.start();
+    }
+}
